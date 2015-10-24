92cbdf7a973184394e24d721fd804b6ab6d3e5cf
Tiff Bogus <tiffany.bogus@gmail.com>
2015-10-23 22:05:11 -0700

    Fix first test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 // var beerSong = function(beerNumber) {
-//   for (var i = 99; i > 0; i -= 99) {
+//   for (var i = 99; i > 0; i -- 99) {
 //     return "99 bottles of beer on the wall, 99 bottles of beer.";
 //   }
 // };
 
-var beerSong = function(beerSong) {
-  if (beerSong === 99) {
+var beerSong = function(n) {
+  for (n = 99; n > 0; n--) {
+    if (n === 99) {
     return "99 bottles of beer on the wall, 99 bottles of beer.";
+    }
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('beerSong', function() {
   it("adds first verse upon number input from user", function() {
     expect(beerSong(99)).to.equal("99 bottles of beer on the wall, 99 bottles of beer.");
   });
+
+  // it("adds second verse, lessening the input integer by one", function() {
+  //   expect(beerSong(99)).to.equal("Take one down and pass it around, 98 bottles of beer on the wall.");
+  // });
 });
