b3e6216c7242634de67f8820a4a06302002b5d71
Tiff Bogus <tiffany.bogus@gmail.com>
2015-10-23 22:48:43 -0700

    Add second test.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,16 @@
 //     return "99 bottles of beer on the wall, 99 bottles of beer.";
 //   }
 // };
-
 var beerSong = function(n) {
-  for (n = 99; n > 0; n--) {
+  for (n < 100; n >= 0; n--) {
     if (n === 99) {
-    return "99 bottles of beer on the wall, 99 bottles of beer.";
+      return (n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + (n - 1) + " bottles of beer on the wall.");
+    } else if (n === 1) {
+      return (n + " bottle of beer on the wall, " + n + " bottle of beer. Take one down and pass it around, " + (n - 1) + " bottles of beer on the wall.");
+    } else {
+      return {
+
+      }
     }
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe('beerSong', function() {
   it("adds first verse upon number input from user", function() {
-    expect(beerSong(99)).to.equal("99 bottles of beer on the wall, 99 bottles of beer.");
+    expect(beerSong(99)).to.equal("99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall.");
   });
 
-  // it("adds second verse, lessening the input integer by one", function() {
-  //   expect(beerSong(99)).to.equal("Take one down and pass it around, 98 bottles of beer on the wall.");
+  it("changes verse when one bottle of beer is left", function() {
+    expect(beerSong(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall.");
+  });
+
+  // it("changes verse again when no beer is left", function() {
+  //   expect(beerSong(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   // });
 });
