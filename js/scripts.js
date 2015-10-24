bc6ac2a95ef25115fbf80def815d005574e68221
Tiff Bogus <tiffany.bogus@gmail.com>
2015-10-23 22:58:42 -0700

    Add third test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
-// var beerSong = function(beerNumber) {
-//   for (var i = 99; i > 0; i -- 99) {
-//     return "99 bottles of beer on the wall, 99 bottles of beer.";
-//   }
-// };
 var beerSong = function(n) {
   for (n < 100; n >= 0; n--) {
     if (n === 99) {
       return (n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + (n - 1) + " bottles of beer on the wall.");
     } else if (n === 1) {
       return (n + " bottle of beer on the wall, " + n + " bottle of beer. Take one down and pass it around, " + (n - 1) + " bottles of beer on the wall.");
-    } else {
-      return {
-
-      }
+    } else if (n === 0) {
+      return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
     }
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -7,7 +7,7 @@ describe('beerSong', function() {
     expect(beerSong(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall.");
   });
 
-  // it("changes verse again when no beer is left", function() {
-  //   expect(beerSong(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
-  // });
+  it("changes verse again when no beer is left", function() {
+    expect(beerSong(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
 });
